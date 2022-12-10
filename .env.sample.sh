239e4b0fbc3f9efbd2ddbe2c1f02d60f4ff01040
Roger Ramos <roger.ramos@gdpteam.com>
2022-12-10 04:17:52 -0500

    ci: dynamic env, flow push docker image

---
+++ .env.sample.sh
@@ -0,0 +1,6 @@
+export ENV=DEV
+export CHAINS_ID='0x13881', '0x5', '0x539'
+export PROVIDER_URL=https://rpc.ankr.com/eth_goerli
+export API=http://ec2-34-238-181-64.compute-1.amazonaws.com
+export IPFS_FILES_URL=https://ipfs.io/ipfs
+export CONTRACT_FORCE_ORIGINAL=false
