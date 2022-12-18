dd88fe3852ef6650e9738c20e8405810eb6ddfa6
Roger Ramos <roger.ramos@gdpteam.com>
2022-12-18 17:08:41 -0500

    fix[dockerfile]: reduce la velocidad del pipeline

--- __contract.make.js
+++ __contract.make.js
@@ -2,8 +2,11 @@ const artifact = require("./contract-artifact.json");
 const {readFileSync, promises: fsPromises} = require('fs');
 
 const templatePath = "./ci/templates/__contract.js";
+
 const artifactStr = JSON.stringify(artifact);
+
 const contents = readFileSync(templatePath, 'utf-8');
+
 const result = contents.replace(
   "JSON.parse(\"${CONTRACT_ARTIFACT_STR}\")",
   artifactStr,
